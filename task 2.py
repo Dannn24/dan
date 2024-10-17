bc1462438a2c533609b5ee7da88664cd5de20330
Dansoy <markdaniellanuebo@gmail.com>
2024-10-17 09:32:28 +0800

    Add files via upload

---
+++ task 2.py
@@ -0,0 +1,30 @@
+
+single_dim_array = [1, 2, 3, 4, 5,]
+two_dim_array = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [10,11,12]
+]
+    
+
+def display_single_array(arr):
+    print("single-dimensional array elements : ")
+    for element in arr:
+        print(element)
+
+def display_two_dimensional_array(arr):
+    print("two-dimensional array elements : ")
+    for sublist in arr:
+        print(sublist)
+
+choice = input("Which array you want to display? ( 1 for single , 2 for two-dimensional): ")
+
+if choice == "1":
+    display_single_dim_array(single_dim_array)
+elif choice == "2":
+    display_two_dim_array(two_dim_array)
+else:
+    print("invalid choice")           
+                
+
